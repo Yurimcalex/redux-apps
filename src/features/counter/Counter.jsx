4ec9bfe69c163df86b7dce8b0d609fbdef8d25cf
Yuri Sklyarov <yuri.mcalex@gmail.com>
2024-05-12 19:45:54 +0300

    Style Counter component

--- src/features/counter/Counter.jsx
+++ src/features/counter/Counter.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { increment, decrement } from './counterSlice.js';
+import styles from './Counter.module.css';
 
 export function Counter() {
 	const count = useSelector(state => state.counter.value);
 	const dispatch = useDispatch();
 
-	
+
 
 	return (
-		<div>
+		<div className={styles.counter}>
 			<div>
 				<button
 					aria-label='Increment value'
