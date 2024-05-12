3d76c0ae9eb9fd737e44fd1c1c1fd09fe150ef0e
Yuri Sklyarov <yuri.mcalex@gmail.com>
2024-05-12 18:09:24 +0300

    Clean the project from template examples

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,10 @@
 import "./App.css";
-//import { Counter } from "./features/counter/Counter";
-//import { Quotes } from "./features/quotes/Quotes";
-//import {} from "./basicExample/basicExample.js";
-//import {} from "./legacyExample/legacyExample.js";
-//import {} from "./todoExample/legacyExample.js";
-import {} from "./todoExample/modernExample.js";
 
 
 const App = () => {
   return (
     <div>
       Ok
-      {/*<Counter />*/}
-      {/*<Quotes />*/}
     </div>
   );
 };
