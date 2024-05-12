c47a481b8a664e5bba2b6e2d1fc1c7cc0edf8971
Yuri Sklyarov <yuri.mcalex@gmail.com>
2024-05-12 22:40:23 +0300

    Create a ui that implements increment by amount

--- src/features/counter/Counter.jsx
+++ src/features/counter/Counter.jsx
@@ -1,13 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { increment, decrement } from './counterSlice.js';
+import { increment, decrement, incrementByAmount } from './counterSlice.js';
 import styles from './Counter.module.css';
 
 export function Counter() {
 	const count = useSelector(state => state.counter.value);
 	const dispatch = useDispatch();
-
-
+	const [incrementAmount, setIncrementAmount] = useState(2);
 
 	return (
 		<div className={styles.counter}>
@@ -24,6 +23,16 @@ export function Counter() {
 					onClick={() => dispatch(decrement())}
 				>
 					Decrement
+				</button>
+				<input type="text"
+					aria-label="Set increment amount"
+				 	value={incrementAmount}
+				 	onChange={(e) => setIncrementAmount(+e.target.value)} 
+				/>
+				<button 
+					onClick={() => dispatch(incrementByAmount(incrementAmount))}
+				>
+					Add amount
 				</button>
 			</div>
 		</div>
