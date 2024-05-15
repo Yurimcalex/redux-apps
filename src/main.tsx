07471dd769abf11662918c441744835fd43108b6
Yuri Sklyarov <yuri.mcalex@gmail.com>
2024-05-15 20:12:52 +0300

    Apply primitiveui css styles

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { Provider } from "react-redux"
 import App from "./App"
 import store from "./app/store"
 import "./index.css"
+import './primitiveui.css'
 import { worker } from './api/server'
 
 async function start() {
