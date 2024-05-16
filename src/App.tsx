e93b25dd1c051ad89effa5c73bca7d09ebde11c7
Yuri Sklyarov <yuri.mcalex@gmail.com>
2024-05-16 19:08:11 +0300

    Add a route for a form that edits a post

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ import { Navbar } from './app/Navbar'
 import { PostsList } from './features/posts/PostsList.jsx';
 import { AddPostForm } from './features/posts/AddPostForm.jsx';
 import { SinglePostPage } from './features/posts/SinglePostPage.jsx';
+import { EditPostForm } from './features/posts/EditPostForm.jsx';
 
 function App() {
   return (
@@ -30,6 +31,7 @@ function App() {
             )}
           />
           <Route exact path="/posts/:postId" component={SinglePostPage} />
+          <Route exact path="/editPost/:postId" component={EditPostForm} />
           <Redirect to="/" />
         </Switch>
       </div>
