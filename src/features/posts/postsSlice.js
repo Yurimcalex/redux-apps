0e91c61ff19bacb4cce0132ad581f9c67f4bf1e0
Yuri Sklyarov <yuri.mcalex@gmail.com>
2024-05-18 19:13:26 +0300

    Update postsSlice to track loading state

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -2,22 +2,28 @@ import { createSlice } from '@reduxjs/toolkit';
 import { nanoid } from '@reduxjs/toolkit';
 import { sub } from 'date-fns';
 
-const initialState = [
-	{ 
-		id: '1',
-		title: 'First Post',
-		content: 'Text text',
-		date: sub(new Date(), { minutes: 10 }).toISOString(),
-		reactions: {thumbsUp: 1, hooray: 2, heart: 3, rocket: 4, eyes: 5}
-	},
-	{ 
-		id: '2',
-		title: 'Second Post',
-		content: 'Another text',
-		date: sub(new Date(), { minutes: 5 }).toISOString(),
-		reactions: {thumbsUp: 5, hooray: 4, heart: 3, rocket: 2, eyes: 1}
-	}
-];
+// const initialState = [
+// 	{ 
+// 		id: '1',
+// 		title: 'First Post',
+// 		content: 'Text text',
+// 		date: sub(new Date(), { minutes: 10 }).toISOString(),
+// 		reactions: {thumbsUp: 1, hooray: 2, heart: 3, rocket: 4, eyes: 5}
+// 	},
+// 	{ 
+// 		id: '2',
+// 		title: 'Second Post',
+// 		content: 'Another text',
+// 		date: sub(new Date(), { minutes: 5 }).toISOString(),
+// 		reactions: {thumbsUp: 5, hooray: 4, heart: 3, rocket: 2, eyes: 1}
+// 	}
+// ];
+
+const initialState = {
+	posts: [],
+	status: 'idle',
+	error: null
+};
 
 const postsSlice = createSlice({
 	name: 'posts',
@@ -25,7 +31,7 @@ const postsSlice = createSlice({
 	reducers: {
 		postAdded: {
 			reducer(state, action) {
-				state.push(action.payload)
+				state.posts.push(action.payload)
 			},
 			prepare(title, content, userId) {
 				return {
@@ -42,7 +48,7 @@ const postsSlice = createSlice({
 		},
 		postUpdated(state, action) {
 			const { id, title, content } = action.payload;
-			const existingPost = state.find(post => post.id === id);
+			const existingPost = state.posts.find(post => post.id === id);
 			if (existingPost) {
 				existingPost.title = title;
 				existingPost.content = content;
@@ -50,7 +56,7 @@ const postsSlice = createSlice({
 		},
 		reactionAdded(state, action) {
 			const { postId, reaction } = action.payload;
-			const existingPost = state.find(post => post.id === postId);
+			const existingPost = state.posts.find(post => post.id === postId);
 			if (existingPost) {
 				existingPost.reactions[reaction]++;
 			}
@@ -60,8 +66,8 @@ const postsSlice = createSlice({
 
 export const { postAdded, postUpdated, reactionAdded } = postsSlice.actions;
 
-export const selectAllPosts = state => state.posts;
+export const selectAllPosts = state => state.posts.posts;
 export const selectPostById = (state, postId) =>
-	state.posts.find(post => post.id === postId);
+	state.posts.posts.find(post => post.id === postId);
 
 export default postsSlice.reducer;
