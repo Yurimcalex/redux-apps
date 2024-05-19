bad64cf08589bee5f07b823c480826ba6d1babe0
Yuri Sklyarov <yuri.mcalex@gmail.com>
2024-05-19 17:18:17 +0300

    Update PostsList to handle posts loading process

--- src/features/posts/PostsList.jsx
+++ src/features/posts/PostsList.jsx
@@ -3,6 +3,7 @@ import { useSelector, useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { PostAuthor } from './PostAuthor.jsx';
 import { selectAllPosts, fetchPosts } from './postsSlice.js';
+import { Spinner } from '../../components/Spinner.jsx';
 
 const PostExcerpt = ({ post }) => {
 	return (
@@ -21,6 +22,7 @@ export const PostsList = () => {
 	const dispatch = useDispatch();
 	const posts = useSelector(selectAllPosts);
 	let postStatus = useSelector(state => state.posts.status);
+	const error = useSelector(state => state.posts.error);
 
 	useEffect(() => {
 		if (postStatus === 'idle') {
@@ -29,13 +31,19 @@ export const PostsList = () => {
 		}
 	}, [postStatus, dispatch]);
 
-	const renderedPosts = posts.map(post => 
-		<PostExcerpt post={post} />);
-
+	let content;
+	if (postStatus === 'loading') {
+		content = <Spinner text='Loading...' />;
+	} else if (postStatus === 'succeeded') {
+		content = posts.map(post => <PostExcerpt post={post} />);
+	} else if (postStatus === 'failed') {
+		content = <div>{error}</div>;
+	}
+	
 	return (
 		<section className='posts-list'>
 			<h2>Posts</h2>
-			{renderedPosts}
+			{content}
 		</section>
 	);
 };
