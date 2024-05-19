277b7f56ff494cf98bcae4f9739f8424a531e2e0
Yuri Sklyarov <yuri.mcalex@gmail.com>
2024-05-19 17:47:24 +0300

    Update the users slice to fetch users from an api

--- src/features/users/usersSlice.js
+++ src/features/users/usersSlice.js
@@ -1,4 +1,4 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 
 const initialState = [
 	{ id: 0, name: 'Bob Hugo' },
@@ -9,7 +9,19 @@ const initialState = [
 const usersSlice = createSlice({
 	name: 'users',
 	initialState,
-	reducers: {}
+	reducers: {},
+	extraReducers(builder) {
+		builder
+			.addCase(fetchUsers.fulfilled, (state, action) => {
+				return action.payload;
+			});
+	}
+});
+
+export const fetchUsers = createAsyncThunk('users/fetchUsers', async () => {
+	const response = await fetch('https://jsonplaceholder.typicode.com/users');
+	const data = await response.json();
+	return data;
 });
 
 export default usersSlice.reducer;
