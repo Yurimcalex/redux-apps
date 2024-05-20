4e8d3efdf4b28f475927e6b53274fbf0a181d854
Yuri Sklyarov <yuri.mcalex@gmail.com>
2024-05-20 19:43:07 +0300

    Update the Navbar to add a notifications tab

--- src/app/Navbar.jsx
+++ src/app/Navbar.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { fetchComments } from '../features/comments/commentsSlice.js';
 
 export const Navbar = () => {
+  const dispatch = useDispatch();
+
+  const fetchNewComments = () => {
+    dispatch(fetchComments());
+  };
+
   return (
     <nav>
       <section>
@@ -11,7 +19,11 @@ export const Navbar = () => {
           <div className="navLinks">
             <Link to="/">Posts</Link>
             <Link to="/users">Users</Link>
+            <Link to="/comments">Comments</Link>
           </div>
+          <button className='button' onClick={fetchNewComments}>
+            Refresh Comments
+          </button>
         </div>
       </section>
     </nav>
