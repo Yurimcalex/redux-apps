482404627da9baa802473d1fa33ea4d5762112a7
Yuri Sklyarov <yuri.mcalex@gmail.com>
2024-05-20 10:34:35 +0300

    Create selectors to select the users slice related data

--- src/features/users/usersSlice.js
+++ src/features/users/usersSlice.js
@@ -18,6 +18,10 @@ const usersSlice = createSlice({
 	}
 });
 
+export const selectAllUsers = state => state.users;
+export const selectUserById = (state, userId) =>
+	state.users.find(user => user.id == userId);
+
 export const fetchUsers = createAsyncThunk('users/fetchUsers', async () => {
 	const response = await fetch('https://jsonplaceholder.typicode.com/users');
 	const data = await response.json();
